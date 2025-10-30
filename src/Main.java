46d1767623598c5ba78b62f5dbe200dc10938780
EyadMhmd02 <eaa105997@gu.edu.eg>
2025-10-30 18:58:13 +0300

    56-taskname-subtask1
    add method to sum 2 numbers.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        sum(10, 5);
+    }
+
+    public static void sum(int n1, int n2) {
+        System.out.println(n1 + n2);
+    }
+}
